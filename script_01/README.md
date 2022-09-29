fc05f494c009215bfd53b7e5275c45e9bf7ae2ef
Olamilekan <headofstate123@gmail.com>
2022-09-29 10:00:47 +0000

    complete

--- script_01/README.md
+++ script_01/README.md
@@ -1,3 +1,8 @@
 # Script_01
 ## Task
 Create a bash script to run at every hour, saving system memory (RAM) usage to a specified file and at midnight it sends the content of the file to a specified email address, then starts over for the new day.
+
+## Solutions
+I create the bash to get the system memory info every hour with a *cronjob* and save it to a log file then a function get call in the bash script if it midnight to send a mail that contain the log file as the body to the address of the email I already set in my environment variable.
+Then if the mail function finish successfully it delete the logfile.
+<!-- ```bash ``` -->
--- script_01/main.sh
+++ script_01/main.sh
@@ -1,8 +1,37 @@
 #!/usr/bin/bash
 
-echo "Sarting the Script"
+# get the value from environment variable if set
+email=$EMAIL
+dateinfo=$(date +%A)
+memoryinfo=($dateinfo-meminfo.log)
+time=$(date +%H)
 
 # getting the system memory info and add it into a file
-date >> daily-meminfo.txt
-head -n 5 /proc/meminfo >> daily-meminfo.txt
-printf "===========================\n" >> daily-meminfo.txt
+function get_memoinfo() {
+    echo "Getting the memory info log for $(date)" >> $memoryinfo
+    printf "===============================================================\n" >> $memoryinfo
+    head -n 5 /proc/meminfo >> $memoryinfo
+    printf " \n" >> $memoryinfo
+}
+
+# this send the mail when called 
+function mailSender() (
+    sendmail $email < $memoryinfo
+)
+
+# This is a function caller that get all operation to get done
+function funcCaller() {
+    get_memoinfo
+
+    if [ ${time} -eq 00 ];
+        then mailSender
+        # remove the log file if the mailsendSender return 0
+            if [ $? -eq 0 ];
+                then rm -rf $memoryinfo
+            fi
+    else
+        :
+    fi
+}
+
+funcCaller
